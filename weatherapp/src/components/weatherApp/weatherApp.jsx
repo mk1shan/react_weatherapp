0fafda0d929df248ac0b901623cc235da078b4a2
Mudipa kishan imayanga <128371632+mk1shan@users.noreply.github.com>
2023-12-14 22:33:12 +0530

    Update weatherApp.jsx

--- weatherapp/src/components/weatherApp/weatherApp.jsx
+++ weatherapp/src/components/weatherApp/weatherApp.jsx
@@ -31,7 +31,7 @@ const temparature =document.getElementsByClassName("weather-temp");
 const location = document.getElementsByClassName("weather-location");
 humidity[0].innerHTML= data.main.humidity+ "%";
 wind[0].innerHTML=Math.floor(data.wind.speed)+ "km/h";
-temparature[0].innerHTML=Math.floor( data.main.temp)+"°c";
+temparature[0].innerHTML= data.main.temp+"°c";
 location[0].innerHTML=data.name;
 
 if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n")
